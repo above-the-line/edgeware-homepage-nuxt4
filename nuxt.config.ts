add557152407ee49bbcb6f0e7dd0f981908fb1aa
above-the-line <play.right.productions@gmail.com>
2026-05-23 23:23:29 +1000

    fixing deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false, 
+  app: {
+    baseURL: '/edgeware-homepage-nuxt4/' 
+  },
+  nitro: {
+    preset: 'github-pages'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
    modules: [
